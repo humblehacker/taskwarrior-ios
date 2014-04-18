7fcef33ab9f688801e2cb4276773573598310667
David Whetstone <david@humblehacker.com>
2014-04-18 04:43:59 -0700

    initial commit

---
+++ taskwarrior-ios/HHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HHAppDelegate.h
+//  taskwarrior
+//
+//  Created by David Whetstone on 4/18/14.
+//  Copyright (c) 2014 humblehacker.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ taskwarrior-ios/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  taskwarrior
+//
+//  Created by David Whetstone on 4/18/14.
+//  Copyright (c) 2014 humblehacker.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HHAppDelegate class]));
+    }
+
+}
